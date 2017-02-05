a6289d4de83050ab875bf82ce8162e77bcf81727
Jamp <jampmil@yahoo.com>
2017-02-05 20:45:54 +0100

    Removing unused files

--- source/session1/setclass.bat
+++
@@ -1,3 +0,0 @@
-set LUCENEDIR=C:\Program Files\Lucene\lucene-3.6.2
-set LUKEDIR=C:\Program Files\Lucene
-set CLASSPATH=.;%LUCENEDIR%\lucene-core-3.6.2.jar;%LUCENEDIR%\contrib\demo\lucene-demo-3.6.2.jar;%LUKEDIR%\lukeall-3.5.0.jar;%CLASSPATH%
--- source/session1/zipit.sh
+++
@@ -1,3 +0,0 @@
-zip session1.zip 20_newsgroups.tar.gz novels.zip setclass setclass.bat lab1.pdf CountWords3.java eclipse-netbeans.txt
-mv session1.zip ../
-
